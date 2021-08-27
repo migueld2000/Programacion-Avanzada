efdb3a179619ff6a44c3f079e82b5ec79638b60b
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-08-27 07:25:02 -0500

    funcion de flecha

---
+++ taller1/flecha.js
@@ -0,0 +1,12 @@
+// metodo 1
+/*function saludar(nombre)
+{
+    return("HOLA " + nombre)
+}
+
+console.log(saludar("juan"))*/
+
+// metodo 2
+
+let saludar=nombre=>("HOLA " + nombre) 
+console.log(saludar("juan"))
