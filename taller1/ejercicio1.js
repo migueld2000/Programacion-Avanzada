07f783134fecbd568b3ab84436f77bfc49990d9c
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-08-26 22:32:30 -0500

    ejercicio1

---
+++ taller1/ejercicio1.js
@@ -0,0 +1,26 @@
+let coordenadax1=0;
+let coordenadax2=50;
+let coordenaday1=0;
+let coordenaday2=-90;
+
+//let restax=cordenadax2-cordenadax1;
+//let restay=cordenaday2-cordenaday2;
+
+//let potenciax=Math.pow(restax,2);
+//let potenciay=Math.pow(restay,2);
+
+//let suma=potenciax+potenciay;
+
+let raiz=Math.sqrt(Math.pow(coordenadax2-coordenadax1,2)+Math.pow(coordenaday2-coordenaday1,2));
+
+console.log("La distancia del plante NABOO se encuantra a " + raiz + " años luz ")
+
+//ENTRADAS
+let coordenadax_1=0;
+let coordenadax_2=50;
+let coordenaday_1=0;
+let coordenaday_2=-70;
+//SALIDA
+console.log(`La distancia del planeta Yavin4 a Naboo es de: ${Math.sqrt(Math.pow(coordenadax_2-coordenadax_1,2)+Math.pow(coordenaday_2-coordenaday_1,2))} UA`);
+
+
