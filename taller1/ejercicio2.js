efadd86342e698d3afcb22ccf1c7f89a02ec6987
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-08-26 22:35:02 -0500

    ejercicio2 V1

---
+++ taller1/ejercicio2.js
@@ -0,0 +1,21 @@
+function CodigoNave (Codigo_Nave,separacionCadena) 
+{
+    let separacionCadena=Codigo_Nave.split(":");
+    Codigo_Nave=CodigoNave;
+    console.log("El piloto de la nave es: "+ separacionCadena[1]);
+}
+
+CodigoNave("ARQ2555:Sara Bel-Sun");
+
+
+/*let codigoNave = "ARQ255:Juan";
+let separacionCadena = codigoNave.split(":");
+console.log(separacionCadena[1]);*/
+/*let codigo_nave1="ARQ2555";
+    let codigo_nave2="ARQ2556";
+    let codigo_nave3="ARQ2557";
+
+    let Piloto;
+    let piloto1="Sara Bel-Sun";
+    let piloto2="Nodin Chavdri";
+    let piloto3="Finn";*/
