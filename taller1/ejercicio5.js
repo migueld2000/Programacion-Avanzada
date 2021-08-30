fdbaa3108ea43654d368384ba42bdb3a1e1d38bb
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-08-30 11:20:42 -0500

    ejercicio5

---
+++ taller1/ejercicio5.js
@@ -0,0 +1,21 @@
+// primer metodo
+function salario_mensual(salario,comision)
+{
+    let total=(salario + comision)-0.05;
+    console.log("El salario mensual del bendedor es de: " + total)
+}
+
+salario_mensual(3500000,1500000);
+
+
+//segundo metodo
+
+let salario=3500000;
+
+let comision=1500000;
+
+let salario_mensual2=(salario,comision)=>salario+comision-0.05;
+
+console.log("El salario mensual del bendedor es de: " + salario_mensual2(3500000,1500000));
+
+
