8037ce31fb9817e2b820c79eb3d8d60955a25274
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-09-09 17:58:00 -0500

    proceso del ejercicio

--- taller1/ejercicio2.js
+++ taller1/ejercicio2.js
@@ -1,12 +1,17 @@
-function CodigoNave (Codigo_Nave) 
-{
-    let separacionCadena=Codigo_Nave.split(":");
-    Codigo_Nave=CodigoNave;
-    console.log("El piloto de la nave es: "+ separacionCadena[1]);
+//Metodo 1
+function CodigoNave(Codigo_Nave) {
+  let separacionCadena = Codigo_Nave.split(":");
+  Codigo_Nave = CodigoNave;
+  console.log("El piloto de la nave es: " + separacionCadena[1]);
 }
 
 CodigoNave("ARQ2555:Sara Bel-Sun");
 
+//Metodo 2
+
+let codigoNave2 = (Codigo_Nave2, separacionCadena2) => separacionCadena2 = Codigo_Nave2.split(":");
+console.log("El piloto de la nave es: " + codigoNave2("ARQ2555:Sara Bel-Sun"+[1])
+);
 
 /*let codigoNave = "ARQ255:Juan";
 let separacionCadena = codigoNave.split(":");
