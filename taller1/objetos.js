20b521d13257bf20f4cdc82be27d9c37069efbe6
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-09-09 17:57:27 -0500

    objetos o cajones

---
+++ taller1/objetos.js
@@ -0,0 +1,27 @@
+let nombre="BLACK WIDOW";
+let director="ALFONSO CUARON";
+let sinopsis="Historia de la viuda negra";
+let genero= "ACCION";
+let duracion=120;
+console.log(genero);
+//ARREGLOS (VECTOR)
+let cajonera=["BLACK WIDOW","ALFONSO CUARON","Historia de la viuda negra","ACCION",120];
+let cajonera2=Array("BLACK WIDOW","ALFONSO CUARON","Historia de la viuda negra","ACCION",120);
+console.log(cajonera2[3]);
+
+//objeto
+
+let pelicula={
+
+    nombre="BLACK WIDOW",
+    director="ALFONSO CUARON",
+    sinopsis="Historia de la viuda negra",
+    genero= "ACCION",
+    duracion=120,
+    personajes:["viuda negra", "la estrella roja"],
+    ciudades:{
+        ciudad1:"londres",
+        ciudad2:"Moscu"
+    }
+}
+console.log(peicula.nombre)
