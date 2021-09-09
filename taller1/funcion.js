65c5673642f26a8b6dda79ad73462113f9298bc3
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-09-09 17:57:08 -0500

    funciones

---
+++ taller1/funcion.js
@@ -0,0 +1,13 @@
+/*function CalcularDistancia(coordenadax1,coordenadax2,
+    coordenaday1,coordenaday2) 
+    {
+        console.log(`La distancia del planeta Yavin4 a Naboo es de: ${Math.sqrt(Math.pow(coordenadax2-coordenadax1,2)+Math.pow(coordenaday2-coordenaday1,2))} UA`);
+    }
+
+CalcularDistancia(0,50,0,-70);
+CalcularDistancia(0,20,0,-30);*/
+
+let str = "";
+let myArr = str.split(":");
+
+console.log(myArr);
