8887e79586ed8be7cc8f00d62d1b7673281b93e9
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-09-09 17:55:48 -0500

    fin del ejercicio 3

--- taller1/ejercicio3.js
+++ taller1/ejercicio3.js
@@ -1,3 +1,4 @@
+//primer metodo
 function Calcular_Promedio_Temperatura(...temperaturas)
 {
     /*let temperaturamaxima=Math.max(temperaturas);
@@ -5,13 +6,17 @@ function Calcular_Promedio_Temperatura(...temperaturas)
     
     let proimedio=(temperaturamaxima+temperaturaminma)/2;*/
 
-    let promedio=((temperaturamaxima=Math.max(...temperaturas)+Math.min(...temperaturas)/2));
+    let promedio=((temperaturamaxima=(Math.max(...temperaturas)+Math.min(...temperaturas))/2));
 
     return (promedio);
-
-
 }
 
 let temperaturas=[61,36,66,55,70];
 
 console.log(`La temperatura promedio fue de: ${Calcular_Promedio_Temperatura(...temperaturas)}°C`)
+
+//segundo metodo
+
+let Calcular_Promedio_Temperatura2=(temperaturas)=>((Math.max((temperaturas)+Math.min(temperaturas))/2));
+console.log("La temperatura promedio fue de: "+Calcular_Promedio_Temperatura2(61,36,66,55,70) + "°C")
+
