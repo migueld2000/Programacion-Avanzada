92ae18e3a2cf2c9431211d8d901a095613669f53
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2021-09-09 17:55:09 -0500

    fin del ejercicio 1

--- taller1/ejercicio1.js
+++ taller1/ejercicio1.js
@@ -1,26 +1,42 @@
-let coordenadax1=0;
-let coordenadax2=50;
-let coordenaday1=0;
-let coordenaday2=-90;
+//Metodo 1
+function allar_cordenadas(
+    coordenadax1,
+    coordenadax2,
+    coordenaday1,
+    coordenaday2
+) {
+    let raiz = Math.sqrt(
+        Math.pow(coordenadax2 - coordenadax1, 2) +
+        Math.pow(coordenaday2 - coordenaday1, 2)
+    );
+    console.log(
+        "La distancia del plante NABOO se encuantra a " + raiz + " años luz "
+    );
+}
 
-//let restax=cordenadax2-cordenadax1;
-//let restay=cordenaday2-cordenaday2;
+allar_cordenadas(0, 50, 0, -90);
 
-//let potenciax=Math.pow(restax,2);
-//let potenciay=Math.pow(restay,2);
-
-//let suma=potenciax+potenciay;
-
-let raiz=Math.sqrt(Math.pow(coordenadax2-coordenadax1,2)+Math.pow(coordenaday2-coordenaday1,2));
-
-console.log("La distancia del plante NABOO se encuantra a " + raiz + " años luz ")
+//Metodo 2
+let allar_cordenadas2 = (
+    coordenadax1,
+    coordenadax2,
+    coordenaday1,
+    coordenaday2
+) =>
+    Math.sqrt(
+        Math.pow(coordenadax2 - coordenadax1, 2) +
+        Math.pow(coordenaday2 - coordenaday1, 2)
+    );
+console.log(
+    "La distancia del plante NABOO se encuantra a " +
+    allar_cordenadas2(0, 50, 0, -90) +
+    " años luz "
+);
 
 //ENTRADAS
-let coordenadax_1=0;
-let coordenadax_2=50;
-let coordenaday_1=0;
-let coordenaday_2=-70;
+//let coordenadax_1=0;
+//let coordenadax_2=50;
+//let coordenaday_1=0;
+//let coordenaday_2=-70;
 //SALIDA
-console.log(`La distancia del planeta Yavin4 a Naboo es de: ${Math.sqrt(Math.pow(coordenadax_2-coordenadax_1,2)+Math.pow(coordenaday_2-coordenaday_1,2))} UA`);
-
-
+//console.log(`La distancia del planeta Yavin4 a Naboo es de: ${Math.sqrt(Math.pow(coordenadax_2-coordenadax_1,2)+Math.pow(coordenaday_2-coordenaday_1,2))} UA`);
