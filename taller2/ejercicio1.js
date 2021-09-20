b8672cbb311dcc1468aa1cea7217d8d7747e6326
miguel david <migueld2000@gmail.com>
2021-09-20 06:15:21 -0500

    cambio asyncawait a una promesa

--- taller2/ejercicio1.js
+++ taller2/ejercicio1.js
@@ -1,21 +1,22 @@
 function reclutar(nombre, planeta, edad, estatura, padawan) {
-  if (edad < 15) {
-    let mensaje = "Manejo de la fuerza";
-    padawan(mensaje, true);
-  } else {
-    let mensaje = "Manejo del sable de luz";
-    padawan(mensaje, false);
-  }
+  let promesa=new Promise(function(resolve, reject) {
+
+    if (edad < 15) {
+      let mensaje = "Manejo de la fuerza";
+      resolve(mensaje,true);
+    } else {
+      let mensaje = "Manejo del sable de luz";
+      reject(mensaje,false);
+    }
+
+  })
+return promesa;
 }
-reclutar("juan", "ABH431", 16, 1.7, function (mensaje, bandera)
-{
-  console.log(mensaje);
-  if (bandera) 
-  {
-    console.log("entrenamiento principal");
-  } 
-  else 
-  {
-    console.log("entrenamiento abansado");
-  }
-});
+reclutar("juan", "ABH431", 16, 1.7,)
+.then(function (respuesta){
+    console.log(respuesta);
+})
+.catch(function (error){
+    console.log(error);
+})
+
